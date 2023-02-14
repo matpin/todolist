b6481a85b1168cd8bfa323181ed130b69d5ff5f9
matpin <stamkouk92@gmail.com>
2023-02-14 21:16:53 +0200

    Adds add and delete handlers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import './App.css';
 import ToDoList from './components/ToDoList';
 import AddToDoButton from './components/AddToDoForm';
@@ -8,6 +8,28 @@ import { getToDoItemsFromLocalStorage, saveTodoItemsToLocalStorage } from './ser
 
 function App() {
   const [todoItems, setTodoItems] = useState(getToDoItemsFromLocalStorage('item') || []);
+
+  const handleAddToDo = useCallback(item => {
+    console.log("addToDo - App2")
+    
+    const items = [
+        {
+        id: item.id,
+        content: item.content,
+        complete: item.complete
+        },
+        ...todoItems,
+    ]
+    setTodoItems(items);
+    saveTodoItemsToLocalStorage('item', items)
+    }, [todoItems]);
+
+
+  const handleOnDelete = useCallback(id => {
+    const newTodoItems = todoItems.filter(item => item.id !== id)
+    setTodoItems(newTodoItems)
+    saveTodoItemsToLocalStorage('item', newTodoItems)
+  }, [todoItems]);
 
   return (
     <div>
