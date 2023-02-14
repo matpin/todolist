7d35478153364362d6bbe1419d5899408400096f
matpin <stamkouk92@gmail.com>
2023-02-14 15:30:55 +0200

    Fixes on todoItems variable and removed the Item import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ToDoList from './components/ToDoList';
-import Item from './components./Item';
 import AddToDoButton from './components/AddToDoForm';
+import { getToDoItemsFromLocalStorage, saveTodoItemsToLocalStorage } from './service/Service';
+
+
 
 function App() {
+  const [todoItems, setTodoItems] = useState(getToDoItemsFromLocalStorage('item') || []);
+
   return (
     <div>
       <AddToDoButton/>
-      <ToDoList toDoItems={toDoItems}/>
+      <ToDoList toDoItems={todoItems}/>
     </div>
   );
 }
