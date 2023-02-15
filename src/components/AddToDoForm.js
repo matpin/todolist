604a4b77b16e91dfb2f1e3468d28e90de48a1f14
matpin <stamkouk92@gmail.com>
2023-02-15 18:28:38 +0200

    Styles form component

--- src/components/AddToDoForm.js
+++ src/components/AddToDoForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // import Item from './Item';
 import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
 
 const AddToDoForm = ({onAddToDo}) => {
     const [ inputValue, setInputValue ] = useState("");
@@ -21,15 +22,53 @@ const AddToDoForm = ({onAddToDo}) => {
         setInputValue("");
     };
 
+    const ItemInput = styled.input`
+  width: 100%;
+  font-size: 22px;
+  padding: 12px 20px;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  `;
+
+  const AddButton = styled.button`
+  width: 100%;
+  background-color: #44bba4;
+  color: white;
+  font-size: 22px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  `;
+
+  const FormArea = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: #24272b;
+  padding: 30px;
+  margin: 10px;
+  color: white;
+  text-align: center;
+  `;
+
+  const Form = styled.form`
+  width: 100%;
+  `;
+
     return(
         <div>
-            <form onSubmit={addToDoItem}>
-                <input 
-                onChange={handleChange}
-                value={inputValue}
-                placeholder='Enter Task'/>
-                <button type='submit'>Add</button>
-            </form>
+            <FormArea>
+                <Form onSubmit={addToDoItem}>
+                    <ItemInput 
+                    onChange={handleChange}
+                    value={inputValue}
+                    placeholder='Enter Task'/>
+                    <button type='submit'>Add</button>
+                </Form>
+            </FormArea>
         </div>
     );
 }
