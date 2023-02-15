8cc8de6d4e49d8dad90a409bebc728a2365146fe
matpin <stamkouk92@gmail.com>
2023-02-15 15:27:30 +0200

    Adds the delete method

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
 
-const Item = ({ id, content, complete }) => {
+const Item = ({ id, content, complete, onDeleteToDo }) => {
+    const handleOnDelete = useCallback(() => 
+        onDeleteToDo(id), [id, onDeleteToDo]
+    );
 
     return (
         <div>
             <p>{content}</p> 
+            <button onClick={handleOnDelete}>X</button>
         </div>
         )
 }
+
+Item.propTypes = {
+    onDeleteToDo: PropTypes.func.isRequired,
+};
 
 export default Item;
